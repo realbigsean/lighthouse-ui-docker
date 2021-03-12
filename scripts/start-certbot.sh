3f7cd9ce7d29d65c600183485b221620cab31b58
realbigsean <seananderson33@gmail.com>
2021-03-12 10:47:56 -0500

    dummy cert fallback, fix config, remove account creation logic

--- scripts/start-certbot.sh
+++ scripts/start-certbot.sh
@@ -5,5 +5,5 @@
 if [ "$ENABLE_SSL" != "" ]; then
 	certonly --webroot --webroot-path=/lighthouse-client-ui --email $EMAIL --agree-tos --no-eff-email --staging -d $NGINX_HOST -d www.$NGINX_HOST
 else
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/letsencrypt/$NGINX_HOST.key -out /etc/letsencrypt/$NGINX_HOST.crt
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/letsencrypt/$NGINX_HOST.key -out /etc/letsencrypt/$NGINX_HOST.crt -batch -subj "/C=PE/ST=Lima/L=Lima/O=Acme Inc. /OU=IT Department/CN=acme.com"
 fi
